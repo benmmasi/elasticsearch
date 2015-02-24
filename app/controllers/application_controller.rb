5f49e4f9f14aee68a41f51fae0971977610c0a93
Ben Masi <ben.m.masi@gmail.com>
2015-02-24 09:20:33 -0500

    Add articles controller and resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+  end
 end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
